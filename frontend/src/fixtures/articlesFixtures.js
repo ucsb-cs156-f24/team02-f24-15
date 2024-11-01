3ec1097983fac6b1e1cb3937d7a6832ae25c4abc
Hao Huan Tang <haohuantang@ucsb.edu>
2024-10-31 19:00:50 -0700

    Adjusted formatting of articlesFixtures using Prettier

--- frontend/src/fixtures/articlesFixtures.js
+++ frontend/src/fixtures/articlesFixtures.js
@@ -1,39 +1,38 @@
 const articlesFixtures = {
-    oneArticle: {
+  oneArticle: {
+    id: 1,
+    title: "My First Article",
+    url: "MyFirstArticle.com",
+    explanation: "This is my first article",
+    email: "StuDent@ucsb.edu",
+    dateAdded: "2024-11-01T01:31:17.613Z",
+  },
+  threeArticles: [
+    {
       id: 1,
       title: "My First Article",
       url: "MyFirstArticle.com",
       explanation: "This is my first article",
       email: "StuDent@ucsb.edu",
-      dateAdded: "2022-01-02T12:00:00",
+      dateAdded: "2024-11-01T01:31:17.613Z",
     },
-    threeArticles: [
-      {
-        id: 1,
-        title: "My First Article",
-        url: "MyFirstArticle.com",
-        explanation: "This is my first article",
-        email: "StuDent@ucsb.edu",
-        dateAdded: "2022-01-02T12:00:00",
-      },
-      {
-        id: 2,
-        title: "My Second Article",
-        url: "MySecondArticle.com",
-        explanation: "This is my Second article",
-        email: "StuDent@ucsb.edu",
-        dateAdded: "2022-01-03T12:00:00",
-      },
-      {
-        id: 3,
-        title: "Hello World",
-        url: "HelloWorld.com",
-        explanation: "A hello world article",
-        email: "AliceBob@ucsb.edu",
-        dateAdded: "2022-02-05T12:00:00",
-      },
-    ],
-  };
-  
-  export { articlesFixtures };
-  
+    {
+      id: 2,
+      title: "My Second Article",
+      url: "MySecondArticle.com",
+      explanation: "This is my Second article",
+      email: "StuDent@ucsb.edu",
+      dateAdded: "2024-11-01T01:31:17.613Z",
+    },
+    {
+      id: 3,
+      title: "Hello World",
+      url: "HelloWorld.com",
+      explanation: "A hello world article",
+      email: "AliceBob@ucsb.edu",
+      dateAdded: "2024-11-01T01:31:17.613Z",
+    },
+  ],
+};
+
+export { articlesFixtures };
