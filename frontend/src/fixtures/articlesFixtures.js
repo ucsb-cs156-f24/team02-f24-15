2f7c0dbf0a2ebd563ba5dfd544487f68a33b87d4
Hao Huan Tang <haohuantang@ucsb.edu>
2024-10-31 18:53:17 -0700

    Added fixtures for Articles

---
+++ frontend/src/fixtures/articlesFixtures.js
@@ -0,0 +1,39 @@
+const articlesFixtures = {
+    oneArticle: {
+      id: 1,
+      title: "My First Article",
+      url: "MyFirstArticle.com",
+      explanation: "This is my first article",
+      email: "StuDent@ucsb.edu",
+      dateAdded: "2022-01-02T12:00:00",
+    },
+    threeArticles: [
+      {
+        id: 1,
+        title: "My First Article",
+        url: "MyFirstArticle.com",
+        explanation: "This is my first article",
+        email: "StuDent@ucsb.edu",
+        dateAdded: "2022-01-02T12:00:00",
+      },
+      {
+        id: 2,
+        title: "My Second Article",
+        url: "MySecondArticle.com",
+        explanation: "This is my Second article",
+        email: "StuDent@ucsb.edu",
+        dateAdded: "2022-01-03T12:00:00",
+      },
+      {
+        id: 3,
+        title: "Hello World",
+        url: "HelloWorld.com",
+        explanation: "A hello world article",
+        email: "AliceBob@ucsb.edu",
+        dateAdded: "2022-02-05T12:00:00",
+      },
+    ],
+  };
+  
+  export { articlesFixtures };
+  
