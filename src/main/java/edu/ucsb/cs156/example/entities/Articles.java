c14614a2e12b41ef9bb6077354651d98a6df7e52
Hao Huan Tang <haohuantang@ucsb.edu>
2024-10-31 18:26:38 -0700

    Added all the backend files from team01

---
+++ src/main/java/edu/ucsb/cs156/example/entities/Articles.java
@@ -0,0 +1,29 @@
+package edu.ucsb.cs156.example.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Entity(name = "articles")
+public class Articles {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    private String title;
+    private String url;
+    private String explanation;
+    private String email; /* (of person that submitted it)*/
+    private LocalDateTime dateAdded;
+}
