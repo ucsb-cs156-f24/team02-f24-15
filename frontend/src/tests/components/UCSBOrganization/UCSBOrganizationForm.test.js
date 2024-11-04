b112cdf763bdb54580f34d725ce5e662bb720988
riasinha <67247334+riasinha@users.noreply.github.com>
2024-11-03 22:19:25 -0800

    pushing to test local errors with npm tests for forms

---
+++ frontend/src/tests/components/UCSBOrganization/UCSBOrganizationForm.test.js
@@ -0,0 +1,117 @@
+import { fireEvent, render, screen, waitFor } from "@testing-library/react";
+import { BrowserRouter as Router } from "react-router-dom";
+
+import UCSBOrganizationForm from "main/components/UCSBOrganization/UCSBOrganizationForm";
+import { ucsbOrganizationFixtures } from "fixtures/ucsbOrganizationFixtures";
+
+import { QueryClient, QueryClientProvider } from "react-query";
+
+const mockedNavigate = jest.fn();
+
+jest.mock("react-router-dom", () => ({
+  ...jest.requireActual("react-router-dom"),
+  useNavigate: () => mockedNavigate,
+}));
+
+describe("UCSBOrganizationForm tests", () => {
+  const queryClient = new QueryClient();
+
+  const expectedHeaders = ["orgTranslationShort", "orgTranslation", "inactive"];
+  const testId = "UCSBOrganizationForm";
+
+  test("renders correctly with no initialContents", async () => {
+    render(
+      <QueryClientProvider client={queryClient}>
+        <Router>
+          <UCSBOrganizationForm />
+        </Router>
+      </QueryClientProvider>,
+    );
+
+    expect(await screen.findByText(/Create/)).toBeInTheDocument();
+
+    expectedHeaders.forEach((headerText) => {
+      const header = screen.getByText(headerText);
+      expect(header).toBeInTheDocument();
+    });
+  });
+
+  test("renders correctly when passing in initialContents", async () => {
+    render(
+      <QueryClientProvider client={queryClient}>
+        <Router>
+          <UCSBOrganizationForm initialContents={ucsbOrganizationFixtures.oneOrganization} />
+        </Router>
+      </QueryClientProvider>,
+    );
+
+    expect(await screen.findByText(/Create/)).toBeInTheDocument();
+
+    expectedHeaders.forEach((headerText) => {
+      const header = screen.getByText(headerText);
+      expect(header).toBeInTheDocument();
+    });
+
+    expect(await screen.findByTestId(`${testId}-orgCode`)).toBeInTheDocument();
+    expect(screen.getByText(`Organization Code`)).toBeInTheDocument();
+  });
+
+  test("that navigate(-1) is called when Cancel is clicked", async () => {
+    render(
+      <QueryClientProvider client={queryClient}>
+        <Router>
+          <UCSBOrganizationForm />
+        </Router>
+      </QueryClientProvider>,
+    );
+    expect(await screen.findByTestId(`${testId}-cancel`)).toBeInTheDocument();
+    const cancelButton = screen.getByTestId(`${testId}-cancel`);
+
+    fireEvent.click(cancelButton);
+
+    await waitFor(() => expect(mockedNavigate).toHaveBeenCalledWith(-1));
+  });
+
+  test("that the correct validations are performed", async () => {
+    render(
+      <QueryClientProvider client={queryClient}>
+        <Router>
+          <UCSBOrganizationForm />
+        </Router>
+      </QueryClientProvider>,
+    );
+
+    /*expect(await screen.findByText(/Create/)).toBeInTheDocument();
+    const submitButton = screen.getByText(/Create/);
+    fireEvent.click(submitButton);
+
+    await screen.findByText(/Name is required/);
+    expect(screen.getByText(/Description is required/)).toBeInTheDocument();
+
+    const nameInput = screen.getByTestId(`${testId}-name`);
+    fireEvent.change(nameInput, { target: { value: "a".repeat(31) } });
+    fireEvent.click(submitButton);
+
+    await waitFor(() => {
+      expect(screen.getByText(/Max length 30 characters/)).toBeInTheDocument();
+    });*/
+
+    const submitButton = await screen.findByText(/Create/);
+    fireEvent.click(submitButton);
+
+    await waitFor(() => {
+      expectedHeaders.forEach(async headerText => {
+        expect(await screen.findByText(`${headerText} is required`)).toBeInTheDocument();
+      });
+    });
+
+    // Check specific field validation example for maximum length
+    /*const shortDescriptionInput = screen.getByTestId(`${testId}-orgTranslationShort`);
+    fireEvent.change(shortDescriptionInput, { target: { value: "a".repeat(256) } }); // Assuming a max length validation
+    fireEvent.click(submitButton);
+
+    await waitFor(() => {
+      expect(screen.getByText(/Max length 255 characters/)).toBeInTheDocument(); // Adjust max length validation message as needed
+    });*/
+  });
+});
