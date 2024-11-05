412bd0fe9c0c20255dc854bd29ca8cf2daf983eb
Cooper Hawley <chawley@ucsb.edu>
2024-11-04 16:39:07 -0800

    Added Fixtures

---
+++ frontend/src/fixtures/ucsbDiningCommonsMenuItemFixtures.js
@@ -0,0 +1,32 @@
+const ucsbDiningCommonsMenuItemFixtures = {
+    oneItem: {
+      id: 1,
+      diningCommonsCode: "DLG",
+      name: "Burger",
+      station: "Grill Station",
+    },
+  
+    threeItems: [
+      {
+        id: 2,
+        diningCommonsCode: "ORT",
+        name: "Pasta",
+        station: "Italian Corner",
+      },
+      {
+        id: 3,
+        diningCommonsCode: "CAR",
+        name: "Tacos",
+        station: "Mexican Station",
+      },
+      {
+        id: 4,
+        diningCommonsCode: "DLG",
+        name: "Salad",
+        station: "Salad Bar",
+      },
+    ],
+  };
+  
+  export { ucsbDiningCommonsMenuItemFixtures };
+  
