86fe5c4bd5d1f15937afb271bf06494487fc13fc
Cooper Hawley <chawley@ucsb.edu>
2024-11-04 16:54:30 -0800

    Hopefully fixed Fixtures, was getting code style issues

--- frontend/src/fixtures/ucsbDiningCommonsMenuItemFixtures.js
+++ frontend/src/fixtures/ucsbDiningCommonsMenuItemFixtures.js
@@ -1,32 +1,31 @@
 const ucsbDiningCommonsMenuItemFixtures = {
-    oneItem: {
-      id: 1,
-      diningCommonsCode: "DLG",
-      name: "Burger",
-      station: "Grill Station",
+  oneItem: {
+    id: 1,
+    diningCommonsCode: "DLG",
+    name: "Burger",
+    station: "Grill Station",
+ },
+  
+  threeItems: [
+    {
+      id: 2,
+      diningCommonsCode: "ORT",
+      name: "Pasta",
+      station: "Italian Corner",
     },
+    {
+      id: 3,
+      diningCommonsCode: "CAR",
+      name: "Tacos",
+      station: "Mexican Station",
+    },
+    {
+      id: 4,
+      diningCommonsCode: "DLG",
+      name: "Salad",
+      station: "Salad Bar",
+    },
+  ],
+};
   
-    threeItems: [
-      {
-        id: 2,
-        diningCommonsCode: "ORT",
-        name: "Pasta",
-        station: "Italian Corner",
-      },
-      {
-        id: 3,
-        diningCommonsCode: "CAR",
-        name: "Tacos",
-        station: "Mexican Station",
-      },
-      {
-        id: 4,
-        diningCommonsCode: "DLG",
-        name: "Salad",
-        station: "Salad Bar",
-      },
-    ],
-  };
-  
-  export { ucsbDiningCommonsMenuItemFixtures };
-  
+export { ucsbDiningCommonsMenuItemFixtures };
