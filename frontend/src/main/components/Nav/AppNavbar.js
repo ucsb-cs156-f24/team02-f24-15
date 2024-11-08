f5d91650ab8c9c2042dd87969cb95560e683be81
Cooper Hawley <chawley@ucsb.edu>
2024-11-07 17:36:33 -0800

    Create Page p2

--- frontend/src/main/components/Nav/AppNavbar.js
+++ frontend/src/main/components/Nav/AppNavbar.js
@@ -69,7 +69,7 @@ export default function AppNavbar({
                   <Nav.Link as={Link} to="/placeholder">
                     Placeholder
                   </Nav.Link>
-                  <Nav.Link as={Link} to="/diningmenucommonsitem">
+                  <Nav.Link as={Link} to="/diningcommonsmenuitem">
                     UCSBDiningCommonsMenuItem
                   </Nav.Link>
                 </>
